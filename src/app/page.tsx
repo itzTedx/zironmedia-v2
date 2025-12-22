57017347e2df015f91a2ac7ed59eb8cf4fb28d97
melwin <114172287+itzTedx@users.noreply.github.com>
2025-12-22 16:30:17 +0400

    chore(deps): remove bun.lock file and update Next.js configuration for improved performance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+
+import { IconArrowRightTag } from "@/assets/icons/arrow";
+import { IconPlay } from "@/assets/icons/play";
 
 export default function Home() {
 	return (
 		<main>
-			<section>
+			<section className="mx-auto max-w-7xl border-x">
 				<div className="mx-auto flex max-w-6xl flex-col items-center gap-4 py-24">
-					<Badge>
+					<Badge className="gap-2">
 						<div className="size-1.5 rounded-full bg-violet-500" /> Ready to
 						grow? We’re ready to go
 					</Badge>
-					<h1 className="text-center font-bold font-display text-8xl uppercase">
+					<h1 className="text-center font-bold font-display text-8xl text-primary uppercase">
 						Turn your brand into a revenue machine
 					</h1>
 					<p className="text-balance text-center font-medium text-2xl text-muted-foreground leading-relaxed">
@@ -20,7 +24,29 @@ export default function Home() {
 						</span>{" "}
 						and everything in between.
 					</p>
+
+					<div className="flex items-center gap-4">
+						<Button className="w-60 justify-between" size="lg">
+							<span>
+								Get started{" "}
+								<span className="font-normal text-primary-secondary">
+									- it’s free
+								</span>
+							</span>
+							<IconArrowRightTag className="size-5" />
+						</Button>
+						<Button
+							className="w-52 justify-between text-muted-foreground"
+							size="lg"
+							variant="secondary"
+						>
+							<span>See us in action</span>
+							<IconPlay className="size-5" />
+						</Button>
+					</div>
 				</div>
+
+				<div />
 			</section>
 		</main>
 	);
--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -9,7 +9,7 @@ import { NAV_LINKS, NavLink } from "./constants";
 export const Navbar = () => {
 	return (
 		<header className="bg-primary py-2">
-			<nav className="mx-auto flex max-w-7xl items-center justify-between gap-4">
+			<nav className="container mx-auto flex max-w-7xl items-center justify-between gap-4">
 				<div className="flex items-center gap-12">
 					<Logo className="size-10" />
 					<ul className="flex items-center gap-12 font-display font-semibold uppercase">
