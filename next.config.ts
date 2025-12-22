ffc18be33612e6707a351283ad09b7449263d3cc
melwin <114172287+itzTedx@users.noreply.github.com>
2025-12-22 11:49:55 +0400

    chore: update configuration files and dependencies for improved setup

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  typedRoutes: true,
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
 };
 
 export default nextConfig;
