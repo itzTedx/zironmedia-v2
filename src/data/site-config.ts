ef96ac749ceca87fabdff8f5b52150c267a9b958
melwin <114172287+itzTedx@users.noreply.github.com>
2025-12-29 17:20:52 +0400

    refactor(layout): update metadata and viewport configuration for site integration

---
+++ src/data/site-config.ts
@@ -0,0 +1,17 @@
+export const siteConfig = {
+	name: "Ziron Media: Digital Marketing Agency for Online Growth ",
+	shortName: "Ziron Media",
+	description:
+		"Ziron Media, a Dubai-based digital marketing agency, offers SEO, PPC, branding, social media management, web design, and lead generation to elevate your business with data-driven strategies.",
+
+	url: "https://www.zironmedia.com",
+	logo: "/favicon.svg",
+	ogImage: "/og.jpg",
+	links: {
+		// twitter: "https://x.com/itzTedx_",
+		linkedin: "https://www.linkedin.com/company/ziron-media",
+		instagram: "https://www.instagram.com/ziron_media/",
+		facebook: "https://www.facebook.com/zironmedia",
+	},
+	contact: "+971 58 171 1486",
+} as const;
