7fe5e585b188aab625ec40d8d0df3e6b0f80e0bb
melwin <114172287+itzTedx@users.noreply.github.com>
2026-01-05 14:10:06 +0400

    refactor(blogs): implement static generation for blog slugs and enhance metadata retrieval

--- src/app/(root)/(resources)/blogs/[slug]/page.tsx
+++ src/app/(root)/(resources)/blogs/[slug]/page.tsx
@@ -1,19 +1,43 @@
+import { Metadata } from "next";
+import Link from "next/link";
+
 import MDXContent from "@/components/markdown/mdx-component";
 
-import { getBlogBySlug } from "@/features/articles/actions/query";
+import { getBlogBySlug, getBlogs } from "@/features/articles/actions/query";
 import { Faq, FaqContent } from "@/features/services/components/faq";
-import Link from "next/link";
+
+export function generateStaticParams() {
+	const blogs = getBlogs();
+	return blogs.map((b) => ({
+		slug: b.slug,
+	}));
+}
+
+export async function generateMetadata({
+	params,
+}: PageProps<"/blogs/[slug]">): Promise<Metadata> {
+	const { slug } = await params;
+	const blog = getBlogBySlug(slug);
+
+	return {
+		title: `${blog.metadata.title} - Ziron Media`,
+		description: blog.metadata.description,
+	};
+}
 
 export default async function BlogPage({ params }: PageProps<"/blogs/[slug]">) {
 	const { slug } = await params;
 
 	const blog = getBlogBySlug(slug);
-	 
+
 	return (
 		<div>
-			<section className="py-9">			 
+			<section className="py-9">
 				<article className="prose prose-stone prose-lg mx-auto max-w-prose prose-a:text-primary prose-a:underline">
-					<MDXContent source={blog.content} components={{a: (props) => <Link {...props}  />, Faq, FaqContent}}/>
+					<MDXContent
+						components={{ a: (props) => <Link {...props} />, Faq, FaqContent }}
+						source={blog.content}
+					/>
 				</article>
 			</section>
 		</div>
